d87ee2b5d408132996f638bf8764d4f325646c6c
Steffen Olson <sknoslo@gmail.com>
2023-12-13 08:14:29 -0500

    day13

--- src/day13.nim
+++ src/day13.nim
@@ -46,8 +46,53 @@ proc partOne(patterns: seq[seq[string]]): string =
 
   $sum
 
+proc isAlmostVerticallyMirroredAbout(pattern: seq[string], x: int): bool =
+  var
+    l = x - 1
+    r = x
+    hasSmudge = false
+
+  while l >= 0 and r < pattern[0].len:
+    for i in 0..<pattern.len:
+      if pattern[i][l] != pattern[i][r]:
+        if hasSmudge:
+          return false
+        hasSmudge = true
+    l -= 1
+    r += 1
+  hasSmudge
+
+proc isAlmostHorizontallyMirroredAbout(pattern: seq[string], y: int): bool =
+  var
+    t = y - 1
+    b = y
+    hasSmudge = false
+
+  while t >= 0 and b < pattern.len:
+    for i in 0..<pattern[0].len:
+      if pattern[t][i] != pattern[b][i]:
+        if hasSmudge:
+          return false
+        hasSmudge = true
+    t -= 1
+    b += 1
+  hasSmudge
+
 proc partTwo(patterns: seq[seq[string]]): string =
-  "INCOMPLETE"
+  var sum = 0
+  
+  for pattern in patterns:
+    block patternCheck:
+      for x in 1..<pattern[0].len:
+        if pattern.isAlmostVerticallyMirroredAbout(x):
+          sum += x
+          break patternCheck
+      for y in 1..<pattern.len:
+        if pattern.isAlmostHorizontallyMirroredAbout(y):
+          sum += y * 100
+          break patternCheck
+
+  $sum
 
 when isMainModule:
   echo "### DAY 13 ###"
