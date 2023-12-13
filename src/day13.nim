77e817070bc6745c5bc90e323f047bd700850920
Steffen Olson <sknoslo@gmail.com>
2023-12-13 08:02:11 -0500

    day13 - part1

---
+++ src/day13.nim
@@ -0,0 +1,62 @@
+import
+  sugar,
+  std/sequtils,
+  std/strutils,
+  aoc2023pkg/benchmark,
+  aoc2023pkg/utils
+
+proc isVerticallyMirroredAbout(pattern: seq[string], x: int): bool =
+  var
+    l = x - 1
+    r = x
+
+  while l >= 0 and r < pattern[0].len:
+    for i in 0..<pattern.len:
+      if pattern[i][l] != pattern[i][r]:
+        return false
+    l -= 1
+    r += 1
+  true
+
+proc isHorizontallyMirroredAbout(pattern: seq[string], y: int): bool =
+  var
+    t = y - 1
+    b = y
+
+  while t >= 0 and b < pattern.len:
+    if pattern[t] != pattern[b]:
+      return false
+    t -= 1
+    b += 1
+  true
+
+proc partOne(patterns: seq[seq[string]]): string =
+  var sum = 0
+  
+  for pattern in patterns:
+    block patternCheck:
+      for x in 1..<pattern[0].len:
+        if pattern.isVerticallyMirroredAbout(x):
+          sum += x
+          break patternCheck
+      for y in 1..<pattern.len:
+        if pattern.isHorizontallyMirroredAbout(y):
+          sum += y * 100
+          break patternCheck
+
+  $sum
+
+proc partTwo(patterns: seq[seq[string]]): string =
+  "INCOMPLETE"
+
+when isMainModule:
+  echo "### DAY 13 ###"
+
+  let input = stdin.readInput
+
+  echo "### INPUT ###"
+  echo input
+  echo "###  END  ###"
+
+  bench(partOne(parsed), partTwo(parsed)):
+    let parsed = input.strip.split("\n\n").map(x => x.splitLines)
