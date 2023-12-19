69972605d2ad238527aa9c683910b1f133b18530
Steffen Olson <solson@relias.com>
2023-12-18 20:27:27 -0500

    day18

--- src/day18.nim
+++ src/day18.nim
@@ -7,6 +7,7 @@ import
 
 type
   Vec2 = tuple[x, y: int]
+  Mat2x2 = tuple[x1, x2, y1, y2: int]
   Instruction = tuple[dir: char, length: int, color: string]
 
 const
@@ -17,6 +18,12 @@ const
 
 proc `+`(l, r: Vec2): Vec2 =
   (l.x + r.x, l.y + r.y)
+
+proc `*`(l: Vec2, r: int): Vec2 =
+  (l.x * r, l.y * r)
+
+proc determinant(mat: Mat2x2): int =
+  mat.x1 * mat.y2 - mat.x2 * mat.y1
 
 proc minMax(lagoon: Table[Vec2, string]): tuple[min, max: Vec2] =
   var
@@ -68,7 +75,6 @@ proc partOne(input: seq[Instruction]): string =
   for inst in input:
     loc = lagoon.dig(loc, inst)
 
-
   let
     mm = lagoon.minMax
     min = mm.min
@@ -107,20 +113,86 @@ proc convert(inst: Instruction): Instruction =
     dir = dirs[i]
   (dir, length, inst.color)
 
+proc outsideLength(instructions: seq[Instruction], i: int): int =
+  # |#
+  # L#########7
+  #          #|
+  #
+  # |#       #|
+  # L#########J
+  #
+  # #|         |#
+  # #L_________J#
+  # #############
+  let
+    pi = if i == 0: instructions.len - 1 else: i - 1
+    ni = if i == instructions.len - 1: 0 else: i + 1
+    curr = instructions[i]
+    prev = instructions[pi]
+    next = instructions[ni]
+
+  if prev.dir == next.dir:
+    return curr.length
+
+  case curr.dir:
+    of 'U':
+      if prev.dir == 'R':
+        return curr.length - 1
+      else:
+        return curr.length + 1
+    of 'R':
+      if prev.dir == 'D':
+        return curr.length - 1
+      else:
+        return curr.length + 1
+    of 'D':
+      if prev.dir == 'L':
+        return curr.length - 1
+      else:
+        return curr.length + 1
+    else:
+      if prev.dir == 'U':
+        return curr.length - 1
+      else:
+        return curr.length + 1
 
 proc partTwo(input: seq[Instruction]): string =
   var
     lagoon: Table[Vec2, string]
-    perimeter = 0
+    loc: Vec2
+    sum = 0
 
-  for inst in input:
-    let realInst = inst.convert
-    perimeter += realInst.length
-    echo $realInst
+  let instructions = input.map(convert)
 
-  echo "perimeter = " & $perimeter & " cubic meters"
+  # Problem space is way too large for the p1 solution, so we'll simplify to calculating just
+  # the corners of the polygon, then we'll google an algorithm for calculating the area :)
+  #
+  # Shoelace formula:
+  # Sum the determinant of the matrices defined by each corner in the trench and then divide by 2
+  #
+  # The tricky bit is discovering if we are inside or outside of the thing. If we start at the
+  # left-most highest point, treat all right turns as being on the outside, and all left turns as
+  # being inside?
+  for (i, inst) in instructions.pairs:
+    let
+      step = case inst.dir:
+        of 'U':
+          UP
+        of 'R':
+          RIGHT
+        of 'D':
+          DOWN
+        else:
+          LEFT
+      scale = instructions.outsideLength(i)
+      nloc = loc + (step * scale)
+      mat = Mat2x2((loc.x, nloc.x, loc.y, nloc.y))
+      det = mat.determinant
 
-  "INCOMPLETE"
+    loc = nloc
+    sum += det
+
+  $(sum div 2)
 
 when isMainModule:
   echo "### DAY 18 ###"
